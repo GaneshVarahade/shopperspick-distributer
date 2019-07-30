f25e5d6c6870dadef8c8b7671f6bed9e4818ea08
Apple <apple@Apples-MacBook.local>
2019-07-30 21:48:30 +0530

    Updated pods

--- Blaze Distribution/Config/EnvironmentVar.swift
+++ Blaze Distribution/Config/EnvironmentVar.swift
@@ -1,6 +1,13 @@
 import Foundation
 
-let API_LOCAL_URL = "http://192.168.1.14:8090"
+let API_LOCAL_URL = "https://api.dev.blaze.me"
 let API_DEV_URL = "https://api.dev.blaze.me"
 let API_STAGE_URL = "https://api.stage.blaze.me"
 let API_PROD_URL = "https://api.blaze.me"
+
+/*
+ let API_LOCAL_URL = "http://192.168.1.14:8090"
+ let API_DEV_URL = "https://api.dev.blaze.me"
+ let API_STAGE_URL = "https://api.stage.blaze.me"
+ let API_PROD_URL = "https://api.blaze.me"
+ */
