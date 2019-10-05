a998f1a2404c123bf212ea1308866970786ce669
Nitin Undare <nitin.undare@thinkitive.com>
2019-10-05 20:04:15 +0530

    1.1 Production relase

--- Blaze Distribution/Config/EnvironmentVar.swift
+++ Blaze Distribution/Config/EnvironmentVar.swift
@@ -1,8 +1,8 @@
 import Foundation
 
-let API_LOCAL_URL = "http://qa.zencanna.io:8092"
-let API_DEV_URL = "http://qa.zencanna.io:8092"
-let API_STAGE_URL = "http://qa.zencanna.io:8092"
+let API_LOCAL_URL = "http://app.zencanna.io:8092"
+let API_DEV_URL = "http://app.zencanna.io:8092"
+let API_STAGE_URL = "http://app.zencanna.io:8092"
 let API_PROD_URL = ""
 
 //let API_LOCAL_URL = "http://qa.shopperspick.io:8092"
