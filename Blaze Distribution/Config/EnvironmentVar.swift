b28320b59d57813eee4c588399329b81be346c5a
Nitin Undare <nitin.undare@thinkitive.com>
2019-10-17 18:45:07 +0530

    V1.3 Production release

--- Blaze Distribution/Config/EnvironmentVar.swift
+++ Blaze Distribution/Config/EnvironmentVar.swift
@@ -1,26 +1,27 @@
 import Foundation
 
-//let API_LOCAL_URL = "http://app.zencanna.io:8092"
-//let API_DEV_URL = "http://app.zencanna.io:8092"
-//let API_STAGE_URL = "http://app.zencanna.io:8092"
-//let API_PROD_URL = ""
+//Production dont change
+let API_LOCAL_URL = "http://app.zencanna.io:8092"
+let API_DEV_URL = "http://app.zencanna.io:8092"
+let API_STAGE_URL = "http://app.zencanna.io:8092"
+let API_PROD_URL = ""
 
 //let API_LOCAL_URL = "http://qa.shopperspick.io:8092"
 //let API_DEV_URL = "http://qa.shopperspick.io:8092"
 //let API_STAGE_URL = "http://qa.shopperspick.io:8092"
 //let API_PROD_URL = ""
 
-let API_LOCAL_URL = "http://qa.zencanna.io:8092"
-let API_DEV_URL = "http://qa.zencanna.io:8092"
-let API_STAGE_URL = "http://qa.zencanna.io:8092"
-let API_PROD_URL = ""
+//let API_LOCAL_URL = "http://qa.zencanna.io:8092"
+//let API_DEV_URL = "http://qa.zencanna.io:8092"
+//let API_STAGE_URL = "http://qa.zencanna.io:8092"
+//let API_PROD_URL = ""
 
 //shopperspicko
 //let API_LOCAL_URL = "http://192.168.7.116:8092"
 //let API_DEV_URL = "http://192.168.7.116:8092"
 //let API_STAGE_URL = "http://192.168.7.116:8092"
 //let API_PROD_URL = ""
-//
+
 
 // let API_LOCAL_URL = "https://api.stage.blaze.me"
 // let API_DEV_URL = "https://api.stage.blaze.me"
